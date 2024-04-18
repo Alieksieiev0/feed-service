27295ca8ee5715d449b2857ecc5dd659f568d17e
Alieksieiev0 <dmitriyalieksieiev@gmail.com>
2024-04-18 19:58:32 +0300

    refactor: unite kafka .go files

--- internal/transport/kafka/producer.go
+++ internal/transport/kafka/producer.go
@@ -8,6 +8,13 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+const (
+	subscriptionTopic = "subscriptions"
+	subscriptionType  = "subscription"
+	postTopic         = "posts"
+	postType          = "post"
+)
+
 type Producer interface {
 	Produce(receivers []types.UserBase, notification Notification) error
 }
@@ -49,4 +56,39 @@ func (p *producer) Produce(receivers []types.UserBase, notification Notification
 	}
 
 	return w.Close()
+}
+
+type Notification interface {
+	Topic() string
+}
+
+type notification struct {
+	FromId   string
+	FromName string
+	TargetId string
+	Type     string
+	topic    string
+}
+
+func (n *notification) Topic() string {
+	return n.topic
+}
+
+func NewPostNotification(ownerId, ownerName, postId string) Notification {
+	return &notification{
+		FromId:   ownerId,
+		FromName: ownerName,
+		TargetId: postId,
+		Type:     postType,
+		topic:    postTopic,
+	}
+}
+
+func NewSubscriptionNotification(userId, userName string) Notification {
+	return &notification{
+		FromId:   userId,
+		FromName: userName,
+		Type:     subscriptionType,
+		topic:    postTopic,
+	}
 }
